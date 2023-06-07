ef91012689f4a4c5b96411851388bc228c651a3a
Your Name <daryagrichenok@gmail.com>
2023-06-07 16:14:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherapp.fragments.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.placeholder, MainFragment.newInstance())
+            .commit()
+    }
+}
